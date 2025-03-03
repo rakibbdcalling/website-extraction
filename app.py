60776ea5ce235cc78ce2e6c71e4981be3db3a454
rakibbdcalling <76678656+rakibbdcalling@users.noreply.github.com>
2025-03-04 04:58:23 +0600

    Update app.py

--- app.py
+++ app.py
@@ -169,4 +169,4 @@ def extract():
 
 if __name__ == "__main__":
     # Only use debug mode in development
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
